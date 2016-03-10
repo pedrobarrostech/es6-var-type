a9dbcd18e69fa018f97ce21234520917b0995e74
Pedro Barros <pedroaugust8@live.com>
2016-03-09 23:08:17 -0300

    Var types

---
+++ const.js
@@ -0,0 +1,10 @@
+// Atribuir valores a uma variável const é um erro de sintaxe
+const a = 1; a = 2;
+
+// const deve ser inicializada
+const foo; // SyntaxError: missing = in const declaration
+
+// const também funciona com objetos
+const myObject = {"key": "value"};
+
+
---
+++ let.js
@@ -0,0 +1,9 @@
+if (x > y) {
+  let g = 12.7 + y;
+  i = g * x;
+}
+
+var i=0;
+for ( let i=i ; i < 10 ; i++ ) {
+  console.log(i);
+}
